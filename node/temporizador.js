8c7e3eff935d198626089c7c4f66a0f6ae87da92
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2021-02-18 19:06:01 -0300

    Atualização

--- node/temporizador.js
+++ node/temporizador.js
@@ -4,10 +4,10 @@ const tarefa1 = schedule.scheduleJob('*/5 * 15 * * 4', function() {
    // console.log('Executando tarefa 1!', new Date().getSeconds())
 })
 
-setTimeout(() => {
-    tarefa1.cancel()
-    console.log('Cancelando Tarefa 1!')
-}, 20000);
+    setTimeout(() => {
+        tarefa1.cancel()
+        console.log('Cancelando Tarefa 1!')
+    }, 20000);
 
 const regra = new schedule.RecurrenceRule()
 regra.dayOfWeek = [new schedule.Range(1, 5)]
